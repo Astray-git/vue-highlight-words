00ec8b1e31f93ce0e77df5ddf536297d0b754dde
Astray-git <lyc001wow@gmail.com>
2018-07-23 18:22:15 +0800

    fix: deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  baseUrl:
+    process.env.NODE_ENV === 'production' ? '/vue-highlight-words/' : '/',
   // https://cli.vuejs.org/guide/build-targets.html#vue-vs-js-ts-entry-files
   configureWebpack: {
     output: {
