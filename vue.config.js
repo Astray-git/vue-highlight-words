2015dca16fa602791797c652dcaf20332c6e3441
Astray-git <lyc001wow@gmail.com>
2019-01-09 14:10:18 +0800

    chore: update deps

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseUrl:
+  publicPath:
     process.env.NODE_ENV === 'production' ? '/vue-highlight-words/' : '/',
   // https://cli.vuejs.org/guide/build-targets.html#vue-vs-js-ts-entry-files
   configureWebpack: {
